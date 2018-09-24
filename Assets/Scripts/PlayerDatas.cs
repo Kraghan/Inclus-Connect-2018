d892042037ad17c178b5a80464d144d3d02d5130
Kraghan <madjojo70@live.fr>
2018-09-25 00:33:19 +0200

    Add initial proto basics

---
+++ Assets/Scripts/PlayerDatas.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public QTEType playerController;
+    public bool enable = false;
+    public bool isDisabledPlayer = false;
+
+    public float skill = 0.5f;
+
+    public float GetInitialSlowMotionFactor()
+    {
+        return 0.5f;
+    }
+}
+
+public class PlayerDatas : MonoBehaviour
+{
+    public PlayerData[] datas = new PlayerData[4];
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public PlayerData GetWeakestPlayer(QTEType[] types)
+    {
+        uint weakestIndex = 0;
+
+        for (uint i = 0; i < datas.Length; ++i)
+        {
+            foreach (QTEType type in types)
+            {
+                if (datas[i].playerController == type
+                    && datas[i].skill < datas[weakestIndex].skill)
+                {
+                    weakestIndex = i;
+                    break;
+                }
+            }
+        }
+
+        return datas[weakestIndex];
+    }
+
+
+}
---
+++ Assets/Scripts/Utility/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TimeManager
+{
+
+    // If slow motion factor == 1 : time is running normaly
+    // If slow motion factor < 1 : time is running slowly
+    // If slow motion factor > 1 : time is running quicky
+    public static void StartSlowMotion(float slowMotionFactor)
+    {
+        Time.timeScale = slowMotionFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    // Stop slow motion effect
+    public static void StopSlowMotion()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+	
+}
