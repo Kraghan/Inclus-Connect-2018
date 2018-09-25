9daa26613b771340ae347379410a81f6def165cb
Kraghan <madjojo70@live.fr>
2018-09-25 14:17:18 +0200

    Normalize code

--- Assets/Scripts/QuickTimeEvent.cs
+++ Assets/Scripts/QuickTimeEvent.cs
@@ -2,36 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum QTEType
+namespace Scripting
 {
-    MICRO,
-    LIGHT,
-    ACCELERO,
-    BUTTON
-}
-
-public class QuickTimeEvent : MonoBehaviour
-{
-
-    public QTEType[] QTENeeded = new QTEType[1];
-    protected static PlayerDatas player = null;
-
-	// Use this for initialization
-	void Start ()
+    public enum QTEType
     {
-        player = GameObject.FindObjectOfType<PlayerDatas>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        PlayerData data = player.GetWeakestPlayer(QTENeeded);
-
-        TimeManager.StartSlowMotion(data.GetInitialSlowMotionFactor());
+        MICRO,
+        LIGHT,
+        ACCELERO,
+        BUTTON
     }
+
+    [RequireComponent(typeof(Collider2D))]
+    public class QuickTimeEvent : MonoBehaviour
+    {
+        [SerializeField]
+        private QTEType[] m_QTENeeded = new QTEType[1];
+        protected static PlayerDatas s_player = null;
+
+        // Use this for initialization
+        void Start()
+        {
+            s_player = GameObject.FindObjectOfType<PlayerDatas>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player"))
+                return;
+
+            PlayerData data = s_player.GetWeakestPlayer(m_QTENeeded);
+
+            Utility.TimeManager.StartSlowMotion(data.GetInitialSlowMotionFactor());
+        }
+    }
+
 }
