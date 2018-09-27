3e19a05d43d6327d6992115dbb1861ece1cd5c3d
Kraghan <madjojo70@live.fr>
2018-09-27 15:18:55 +0200

    Add FX on QTE

---
+++ Assets/Scripts/FX/FogAndFirefliesColorChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogAndFirefliesColorChanger : MonoBehaviour {
+
+    public ParticleSystem p_fog;
+    public ParticleSystem p_fireflies;
+
+    public void ChangeColor(Color color)
+    {
+        ParticleSystem.MainModule fogMain = p_fog.main;
+        ParticleSystem.MainModule firefliesMain = p_fireflies.main;
+        fogMain.startColor = new Color(color.r,color.g, color.b, fogMain.startColor.color.a);
+        firefliesMain.startColor = new Color(color.r, color.g, color.b, firefliesMain.startColor.color.a);
+    }
+}
